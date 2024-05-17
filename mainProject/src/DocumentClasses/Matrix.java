1bc1fca81d4668e568edd922652a7ba8aadf7be8
Tylermhh <tyler.hlaing@gmail.com>
2024-05-17 14:01:27 -0700

    added new files for lab7

---
+++ mainProject/src/DocumentClasses/Matrix.java
@@ -0,0 +1,114 @@
+package DocumentClasses;
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class Matrix {
+    private ArrayList<ArrayList<Integer>> matrix;
+    public Matrix(){
+        this.matrix = new ArrayList<>();
+    }
+
+    public Matrix(ArrayList<ArrayList<Integer>> data){
+        this.matrix = data;
+    }
+
+    private int findFrequency(int attribute, int value, ArrayList<Integer> rows){
+        return 0;
+    }
+
+    private HashSet<Integer> findDifferentValues(int attribute, ArrayList<Integer> rows){
+        HashSet<Integer> result = new HashSet<>();
+        return result;
+    }
+
+    private ArrayList<Integer> findRows(int attribute, int value, ArrayList<Integer> rows){
+        ArrayList<Integer> resultRows = new ArrayList<>();
+
+        return resultRows;
+    }
+
+    private double log2(double number){
+        return Math.log(number) / Math.log(2);
+    }
+
+    // calculating entropy for one of the options when we split on an attribute
+    private double findEntropy(ArrayList<Integer> rows){
+        double entropy = 0;
+        int total_count = 0;
+        HashMap<Integer, Integer> classesCount = new HashMap<>();
+        for (Integer rowNum : rows){
+            ArrayList<Integer> row = this.matrix.get(rowNum);       // go through each of the specified rows
+            // row.get(4) is the class (1,2,3) in our case. but we use a hashmap so we can support diff num of classes
+            classesCount.put(row.get(4), classesCount.getOrDefault(row.get(4), 0) + 1);     // increment the counter for items in the class that the row belongs to
+            total_count++;
+        }
+
+        // go through all identified classes and calculate total entropy
+        for (Map.Entry<Integer, Integer> entry : classesCount.entrySet()){
+            double ratio = (double) classesCount.get(entry.getKey()) / total_count;
+            entropy += ratio * log2(ratio);
+        }
+
+        return entropy;
+    }
+
+    private double findEntropy(int attribute, ArrayList<Integer> rows){
+        double entropy = 0;
+        HashMap<Integer, ArrayList<Integer>> rowsForEachAttributeVal = new HashMap<>();     // stores the row numbers per attribute category
+
+        for (Integer rowNum : rows){
+            ArrayList<Integer> row = this.matrix.get(rowNum);
+            int attributeCategory = row.get(attribute);         // get the cateogory that the row gets put under if split on given attribute
+            if (rowsForEachAttributeVal.containsKey(attributeCategory)){        // if this attribute val is already seen before, just add the row to list of related rows
+                rowsForEachAttributeVal.get(attributeCategory).add(rowNum);
+            } else {        // if it is a new attribute value/category, make a new arraylsit for it
+                ArrayList<Integer> rowsForAttribute = new ArrayList<>();
+                rowsForAttribute.add(rowNum);
+                rowsForEachAttributeVal.put(attributeCategory, rowsForAttribute);
+            }
+        }
+
+        for (Map.Entry<Integer, ArrayList<Integer>> entry : rowsForEachAttributeVal.entrySet()){
+
+        }
+
+        return entropy;
+    }
+
+    private double findGain(int attribute, ArrayList<Integer> rows){
+        return 0;
+    }
+
+    public double computeIGR(int attribute, ArrayList<Integer> rows){
+        return 0;
+    }
+
+    public int findMostCommonValue(ArrayList<Integer> rows){
+        HashMap<Integer, Integer> valuesCounts = new HashMap<>();
+        for (int i=0; i<this.matrix.size(); i++){
+            if (rows.contains(i)){      // to ignore the rows not specified in parameter
+                ArrayList<Integer> curRow = this.matrix.get(i);     // get the row
+                for (Integer value : curRow){       // loop through values in that row
+                    if (valuesCounts.containsKey(value)){
+                        valuesCounts.put(value, valuesCounts.get(value) + 1);       // increment count of that value
+                    } else{
+                        valuesCounts.put(value, 1);     // add value into hashmap to keep count
+                    }
+                }
+            }
+        }
+
+        return Collections.max(valuesCounts.entrySet(), Map.Entry.comparingByValue()).getKey(); // return the max counted key
+
+    }
+
+    public HashMap<Integer, ArrayList<Integer>> split(int attribute, ArrayList<Integer> rows){
+        HashMap<Integer, ArrayList<Integer>> result = new HashMap<>();
+        return result;
+    }
+
+    public String toString(){
+        return this.matrix.toString();
+    }
+}
--- mainProject/src/labs/Lab7.java
+++ mainProject/src/labs/Lab7.java
@@ -1,4 +1,51 @@
 package labs;
 
+import DocumentClasses.ItemSet;
+import DocumentClasses.Matrix;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Lab7 {
+    public static void main (String[] args){
+        Matrix data = new Matrix(process("D:\\College\\Com Sci\\Junior Year\\466\\Labs\\mainProject\\src\\InputFiles\\data.txt"));
+        System.out.println(data);
+        ArrayList<Integer> rows = new ArrayList<>();
+        rows.add(0);
+        rows.add(1);
+        rows.add(2);
+        System.out.println(data.findMostCommonValue(rows));
+    }
+
+    public static ArrayList<ArrayList<Integer>> process (String filePath){
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null)
+            {
+                String[] splitLine = line.split(",");
+                ArrayList<Integer> newEntry = new ArrayList<>();
+                for (String elem : splitLine){
+                    Double value = Double.parseDouble(elem);
+                    Integer flooredVal = (int) Math.floor(value);
+                    newEntry.add(flooredVal);
+                }
+                result.add(newEntry);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return result;
+    }
+
+    // TODO: finish function
+    public static void printDecisionTree(int[][] data, ArrayList<Integer> attributes, ArrayList<Integer> rows, int level, double currentIGR){
+
+    }
 }
